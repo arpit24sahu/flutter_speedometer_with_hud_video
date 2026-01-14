1cbd33a6b920b417d02eb028554c54bee53ab233
Arpit Sahu <rptsahu1@gmail.com>
2026-01-14 23:34:59 +0530

    minor changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
-        versionCode = 9
-        versionName = "0.0.6"
+        versionCode = 10
+        versionName = "0.0.7"
     }
 
     signingConfigs {
